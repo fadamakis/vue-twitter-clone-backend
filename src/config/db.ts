236929046555a0cfaac942b7725cc059f8ed3751
Fotis Adamakis <fadamakis@gmail.com>
2024-08-30 13:22:57 +0300

    moved MONGO_URL to env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,23 +1,22 @@
 import mongoose from "mongoose";
- 
+
 export default function connectDB() {
-  const url = "mongodb://localhost:27017";
-  
+  const url = process.env.MONGO_URL;
+
   try {
     mongoose.connect(url);
   } catch (err) {
     console.log(err.message);
     process.exit(1);
   }
-  
+
   const dbConnection = mongoose.connection;
 
   dbConnection.once("open", (_) => {
     console.log(`Database connected: ${url}`);
   });
- 
+
   dbConnection.on("error", (err) => {
     console.error(`connection error: ${err}`);
   });
-  
 }
